e1189a24278d1dcc27cf1de2532eb82b1ef6079f
marco3islas <marco3islas@gmail.com>
2020-03-25 10:34:36 -0600

    Se mejoraron las imagenes, y se añadieron funcionalidades en el js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(function() {
     'use strict';
 
 
-
+    /* Remover y mostrar movil menu y esconder menu */
     $('.movile-menu').on('click', function(e) {
         $(this).addClass('esconder'),
             $('#nav').removeClass('esconder'),
@@ -21,23 +21,53 @@ $(function() {
         $(this).addClass('activo');
     })
 
+    $('main article img').on('mouseenter', aumentarImagen);
+    $('main article img').on('mouseleave', disminuirImagen);
 
+    function aumentarImagen() {
+        $(this).animate({ 'width': '100%' });
+    }
 
-    $('#subNivel').on('mouseover', function() {
+    function disminuirImagen() {
+        $(this).animate({ 'width': '350px' });
+    }
+
+    $('#subNivel').on('click', function() {
         $('.subMenu').slideDown("slow");
     })
-    $('.subMenu').on('mouseleave', function() {
+    $('.subMenu').on('click', function() {
         $(this).slideUp('fast');
     })
     $('#subNivel2').on('mouseover', function() {
         $('.subMenu2').slideDown("slow");
     })
-    $('.subMenu2').on('mouseleave', function() {
+    $('.subMenu2').on('click', function() {
         $(this).slideUp('fast');
     })
     $('.subNivel').on('click', function(e) {
         $(this).addClass('activo');
     })
+
+    /*Link activo*/
+    var linkActivo = location.href;
+    switch (linkActivo) {
+        case 'http://127.0.0.1:5501/index.html':
+            $('.index').addClass('activo');
+            break;
+        case 'http://127.0.0.1:5501/about.html':
+            $('.about').addClass('activo');
+            break;
+        case 'http://127.0.0.1:5501/blog.html':
+            $('.blog').addClass('activo');
+            break;
+        case 'http://127.0.0.1:5501/contacto.html':
+            $('.contacto').addClass('activo');
+            break;
+
+        default:
+            $('.index').addClass('activo');
+            break;
+    }
 
     //
     // ─── NOSOTROS SLIDER ────────────────────────────────────────────────────────────
